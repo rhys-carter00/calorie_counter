d141cd2cc05f701b64259af1697d509eeeecee54
Rhys Carter <rhys.gl.carter@gmail.com>
2025-02-06 15:52:44 +0000

    first draft

---
+++ calorie_counter.js
@@ -0,0 +1,6 @@
+const calculateButton = document.getElementById("calculate-button");
+const breakfastCalories = parseInt(document.getElementById("breakfast-calories").value, 10) || 0;
+
+
+calculateButton.addEventListener("click", () => {
+}
